dfa6e0277e97438336a068acb0da1b69417af989
Bijohloge <bijohloge@gmail.com>
2023-02-21 22:12:03 +0100

    learningapps integriert

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function setNames() {
     this.name = document.getElementById('name-input').value;
     document.getElementById("test").innerHTML = this.name;
 }
+
+function showLearningApp() {
+    var element = document.getElementById("learningapps");
+    element.style.display = "block";
+}
+
+function closeLearningApp() {
+    var element = document.getElementById("learningapps");
+    element.style.display = "none";
+}
