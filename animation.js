3ac5f96cac2f7a745322747514112b09b8d765d8
Bijohloge <116212130+Bijohloge@users.noreply.github.com>
2023-03-07 17:23:29 +0100

    test für hintergrund

--- animation.js
+++ animation.js
@@ -2,8 +2,12 @@ var wheel = setInterval(spinWheel, 10);
 var rotation = 0;
 
 
-function spinWheel() {
+async function spinWheel() {
     var glücksrad = document.getElementById('glücksrad');
     glücksrad.style.transform = "rotate(" + rotation + "deg)",
         rotation += 1.5;
+}
+
+function animateDoor() {
+
 }
