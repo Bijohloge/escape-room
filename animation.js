7f08fa3e8112c95defbd1eec84f5d65e80677716
Bijohloge <116212130+Bijohloge@users.noreply.github.com>
2023-03-09 21:57:23 +0100

    Drehendes Glücksrad

--- animation.js
+++ animation.js
@@ -1,11 +1,27 @@
-//var wheel = setInterval(spinWheel, 10);
-var rotation = 0;
+let start = 0;
+let rotation = 0;
+
+function spinWheel(time) {
+    const elapsed = (time - start) / 1000;
+
+    var glücksrad = document.getElementById('animation-glücksrad');
+    if (!document.contains(glücksrad)) return;
 
 
-async function spinWheel() {
-    var glücksrad = document.getElementById('glücksrad');
-    glücksrad.style.transform = "rotate(" + rotation + "deg)",
-        rotation += 1.5;
+    rotation += 140 * elapsed;
+    if (rotation > 360) {
+        var x = rotation - 360;
+        rotation = x;
+    }
+    glücksrad.style.transform = "rotate(" + Math.round(rotation) + "deg)";
+    start = time;
+    requestAnimationFrame(spinWheel);
+}
+
+requestAnimationFrame(spinWheel);
+
+function animate() {
+
 }
 
 function animateDoor() {
