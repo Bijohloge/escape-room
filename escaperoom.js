18a85de8b859a44050731c12d963c09c7f456f3a
Bijohloge <116212130+Bijohloge@users.noreply.github.com>
2023-03-12 00:23:05 +0100

    Beutel Hinzugefügt

--- escaperoom.js
+++ escaperoom.js
@@ -30,7 +30,7 @@ function onTür() {
     if (window.localStorage.getItem("isDoorOpen") == "True") {
         window.location.href = "https://bijohloge.github.io/escape-room/escaperoom2.html";
     } else {
-        alert("Du musst erst den Code richtig eingeben bevor du Durch die Tür gehen darfst!!!");
+        alert("Der Code ist Falsch!!");
     }
 }
 
