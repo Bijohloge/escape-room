3b91ff05ab6f04b8e9e1977bc376e4f91455c508
Bijohloge <116212130+Bijohloge@users.noreply.github.com>
2023-03-12 18:39:22 +0100

    Tresor bild

--- escaperoom2.js
+++ escaperoom2.js
@@ -1,3 +1,6 @@
 function openKasten() {
-    var kasten = document.getElementById("");
+    var kasten = document.getElementById("Sicherungskasten");
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+    kasten.style.display = "block";
 }
