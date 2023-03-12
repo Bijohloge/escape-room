04557ef0a19d96e53dc82e87579e84218fa2562e
Bijohloge <116212130+Bijohloge@users.noreply.github.com>
2023-03-12 19:51:16 +0100

    sicherung

--- escaperoom2.js
+++ escaperoom2.js
@@ -1,6 +1,24 @@
+var sicherungen = [true, false, true, false, true, false, true];
+
 function openKasten() {
     var kasten = document.getElementById("Sicherungskasten");
     var overlay = document.getElementById("overlay");
     overlay.style.display = "block";
     kasten.style.display = "block";
+}
+
+function sicherungsSchalter(id) {
+
+    var schalter = document.getElementById(id);
+    var num = Number(id) - 1;
+    sicherungen[num] = !sicherungen[num];
+    console.log(sicherungen);
+    var img;
+    if (sicherungen[num]) {
+        img = "images/Sicherung An.svg";
+    } else {
+        img = "images/Sicherung Aus.svg";
+    }
+    schalter.firstElementChild.src = img;
+
 }
